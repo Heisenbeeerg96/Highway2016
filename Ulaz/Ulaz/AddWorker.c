895d22ae6bfd1a5d2fb16ae540bd5274e22b6b4d
bubo2102 <bubo2102@hotmail.com>
2017-01-04 23:25:26 +0100

    uljepsao ispis u datoteku

--- Ulaz/Ulaz/AddWorker.c
+++ Ulaz/Ulaz/AddWorker.c
@@ -7,6 +7,6 @@ void addWorker()
 	file = fopen("zaposleni.txt", "a");
 	if (file)
 	{
-		fprintf(file, "%-10s  %-7s  %-13s  %d.%d.%d\n", p.name, p.surname, p.JMBG, p.birth.dd, p.birth.mm, p.birth.yy);
+		fprintf(file, "%-15s %-23s %-17s %d.%d.%d\n", p.name, p.surname, p.JMBG, p.birth.dd, p.birth.mm, p.birth.yy);
 	}
 }
--- Ulaz/Ulaz/Person.h
+++ Ulaz/Ulaz/Person.h
@@ -1,5 +1,5 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #include "Date.h"
-typedef struct person { char name[10];char surname[20]; char JMBG[20]; DATE birth; }PERSON;
+typedef struct person { char name[20];char surname[20]; char JMBG[20]; DATE birth; }PERSON;
 void initPerson(PERSON*);
