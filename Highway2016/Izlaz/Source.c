b3eb4e5f7b44309ed50ca51f322f80aea82522ef
Milan Carakovac <milancarakovac@yahoo.com>
2017-01-22 18:07:00 +0100

    Zavrsni pregled :D

--- Highway2016/Izlaz/Source.c
+++ Highway2016/Izlaz/Source.c
@@ -14,24 +14,12 @@ int main()
 	//ideja je da program funkcionise na racunaru za kojim operise covjek, a nalazi se u kucici, tako da, imacemo kucicu na ulazu i 
 	//izlazu
 
-	int input = 1, input2, input3 = 1, SOScount = readSOSReport();
-	char d1;
+	int log, SOScount = readSOSReport();
+	char d1, input[2], input2[2], logInput[2];
 	char name[10], dion[10];
 	char password[15];
 	char sifra[6];
-	//ovo dio sam koristio za testiranje,ali po ovome mozete vidjet kako funkcionise,nemorate zalazit dublje u kod
-	/*Podatak tipa BILL sluzi da se ucita(nadje) potvrdu vozacu kada dodje na izlaz,OVO TREBA VIDA KORISTIT ZA MUP
-	 jer se ovim prestavlja potvrda,po pravilu izlaz bi se trebao da se realziuje da pita vozaca da unese sifu pa onda
-	 dionicu na kojoj izlazi,ili obrnuto samo sacuvajte te podatke i posaljite u ove funkcije dole!!!*/
-	/*speedTicket je za pisanje kaznje,mislim slanje potvrde MUPU,kako smo na proslom sastanku pricla MIlan je rekao da se potvrda 
-	 jedino moze sacuvaj kada admin se odjavi,e neka to i sa ovim djelom koda uradi
-	 pod 3,napravio sam da ufunkciji speedticet otvara dadoteku u rezimu ,,a",neka se to promjeni(mislim ako se treba neka se promjenii) da bude u skladu kao 
-	 sa onim dadotekama kako se stapaju na ulazu...u SpeedTicket saljemo potvrdu(to je potvrda o vozacu koji je usao na dionicu,a dion je dionica na kojoj je izasao
-	 pa uklopite to*/
-	//speedTicket(&ob, dion);
-
-	
-	
+		
 	system("CLS");
 	printf("******************HIGHWAY2016*******************\n\n\n");
 	printf("Unesite ime dionice  na kojoj ce se ovaj softver koristiti(A,B,C,D,E,F):");
@@ -49,185 +37,196 @@ int main()
 			printf("2.Pozovi pomoc na autoput!\n");
 			printf("3.Pomoc pri koristenju programa!\n");
 			printf("\n\nUnesite redni broj opcije:");
-			scanf("%d", &input);
-		} while (input > 4 || input < 1);
-		switch (input)
-		{
-		case(1):
-		{
-			do
+			scanf("%s", input);
 			{
-				system("CLS");
-				printf("******************HIGHWAY2016*******************\n\n\n");
-				printf("1.Admin\n2.Operater\n3.Izlaz\nIzaberite opciju:");
-				scanf("%d", &input2);
-			} while (input2 < 1 || input2>3);
-			int countLogs = 0;
-			if (input2 == 3)break;
-			else
-			{
-				do
+				if (strcmp(input, "1") == 0)
 				{
-					system("CLS");
-					printf("******************HIGHWAY2016*******************\n\n\n");
-					printf("Unesite korisnicko ime:");
-					scanf("%s", name);
-					printf("Unesite lozinku:");
-					scanf("%s", password);
-					countLogs++;
-				} while (LoginCheck(name, password, input2) == 0 && countLogs < 4);
-				if (countLogs < 4)
-				{
-					system("CLS");
-					printf("******************HIGHWAY2016*******************\n\n\n");
-					switch (input2)
+					do
 					{
-					case(1):
+						system("CLS");
+						printf("******************HIGHWAY2016*******************\n\n\n");
+						printf("1.Admin\n2.Operater\n3.Izlaz\nIzaberite opciju:");
+						scanf("%s", input2);
+					} while (strcmp(input2, "1") < 0 || strcmp(input2, "3") > 0);
+					int countLogs = 0;
+					if (strcmp(input2, "3") == 0)break;
+					else
 					{
 						do
 						{
-							while (1)
-							{
-								system("CLS");
-								printf("******************HIGHWAY2016*******************\n\n\n");
-								printf("MENI:\n\n");
-								printf("2.Promijeni stanje na putevima!\n");
-								printf("3.Izlistaj trenutna stanja na dionicama!\n");
-								printf("4.Dodaj novog zaposlenog!\n");
-								printf("5.Izlaz iz programa!\n");
-								printf("6.Odjava!\n");
-								printf("Unesite redni broj opcije:");
-								scanf("%d", &input3);
-								 if (input3 == 2)
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									printf("Izmjena na radu!\n");/*ovaj dio koda sluzi za izmjene na putu,unese se dionice koje su direkno povezane i onda imas mogucnost
-																 promjene brzine,stanja itd...,na ulazi i izlaz stavit mogucnost promjene na putu*/
-									printf("Na kojoj putanji se vrsi izmjena(putanja oznacava dvije dionice):\n");
-									char dio1[2], dio2[2];
-									scanf("%s %s", dio1, dio2);//trazi da uneses dvije dionice da pormenis njhova stanja!!
-									changeStat(dio1, dio2);
-								}
-								else if (input3 == 3)
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									printRoadStat();
-									system("pause");
-								}
-								else if (input3 == 4)
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									addWorker();
-								}
-								else if (input3 == 5)
-								{
-									fixSOSReport(SOScount);
-									system("pause");
-									return 0;
-								}
-								else if (input3 == 6)
-								{
-									system("pause");
-									break;
-								}
-							}
-						} while (input3 < 1 || input3 >6);
-						break;
-
-
-
-					}
-					case(2):											
-					{
-						while (1)
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							if (strcmp(input2, "1") == 0)
+								log = 1;
+							else log = 2;
+							printf("Unesite korisnicko ime:");
+							scanf("%s", name);
+							printf("Unesite lozinku:");
+							scanf("%s", password);
+							countLogs++;
+						} while (LoginCheck(name, password, log) == 0 && countLogs < 4);
+						if (countLogs < 4)
 						{
-							do
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							switch (log)
 							{
-								system("CLS");
-								printf("******************HIGHWAY2016*******************\n\n\n");
-								printf("MENI:\n\n");
-								printf("1.Izlistaj stanja na dionicama!\n");
-								printf("2.Provjeri da li ima zahtjeva za pomoc!\n");
-								printf("3.Naplati!\n");
-								printf("4.Odjava!\n");
-								printf("Unesite redni broj opcije: ");
-								scanf("%d", &input3);
-							} while (input3 < 1 || input3 >4);
-							if (input3 == 1)
+							case(1):
 							{
-								system("CLS");
-								printf("******************HIGHWAY2016*******************\n\n\n");
-								printRoadStat();
-								{
-									printf("Pomoc poslata!\n");
-									SOScount--;
-									system("pause");
-								}
-							}
-							else if (input3 == 3)
-							{
-								system("CLS");
-								printf("******************HIGHWAY2016*******************\n\n\n");
-								char sifra[6];
-								BILL ob;
 								do
 								{
-									printf("Unesite sifru vozaceve potvrde:\n");
-									scanf("%s", sifra);//sifra prestavlja sifru potvrde koju dobije korisnik na pocetku
-									ob = findPassword(sifra); //trazi sifru u dadoteci potvrda,i kada nadje samo inicilazuje podatak u mainu
-
-								} while (ob.year == -1);//ovo koristim ako vozac slucajno unese pogresnu sifru,pa da ima mogucnost opet unosa
-								printf("Sifra je pronadjenja:%s\n", ob.password); //pronalaz potvrde!
-								finallprice(&ob, dion); //proracun cjene!
-								speedTicket(&ob, dion);
-								printf("Hvala Vam ste koristili nase usluge!\nSretan put\n");//ispis na displeju sa vanjske strane kucice
-								system("pause");
-							}
-							else if (input3 == 4)
-							{
-								system("pause");
+									while (1)
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										printf("MENI:\n\n");
+										printf("1.Promijeni stanje na putevima!\n");
+										printf("2.Izlistaj trenutna stanja na dionicama!\n");
+										printf("3.Dodaj novog zaposlenog!\n");
+										printf("4.Izlaz iz programa!\n");
+										printf("5.Odjava!\n");
+										printf("Unesite redni broj opcije:");
+										scanf("%s", logInput);
+										if (strcmp(logInput, "1") == 0)
+										{
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											printf("Izmjena na radu!\n");/*ovaj dio koda sluzi za izmjene na putu,unese se dionice koje su direkno povezane i onda imas mogucnost
+																		 promjene brzine,stanja itd...,na ulazi i izlaz stavit mogucnost promjene na putu*/
+											printf("Na kojoj putanji se vrsi izmjena(putanja oznacava dvije dionice):\n");
+											char dio1[2], dio2[2];
+											scanf("%s %s", dio1, dio2);//trazi da uneses dvije dionice da pormenis njhova stanja!!
+											changeStat(dio1, dio2);
+										}
+										else if (strcmp(logInput, "2") == 0)
+										{
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											printRoadStat();
+											system("pause");
+										}
+										else if (strcmp(logInput, "3") == 0)
+										{
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											addWorker();
+										}
+										else if (strcmp(logInput, "4") == 0)
+										{
+											fixSOSReport(SOScount);
+											system("pause");
+											return 0;
+										}
+										else if (strcmp(logInput, "5") == 0)
+										{
+											system("pause");
+											break;
+										}
+									}
+								} while (strcmp(logInput, "5") != 0 && (strcmp(logInput, "1") < 0 || strcmp(logInput, "5") > 0));
 								break;
+
+
+
+							}
+							case(2):
+							{
+								while (1)
+								{
+									do
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										printf("MENI:\n\n");
+										printf("1.Izlistaj stanja na dionicama!\n");
+										printf("2.Provjeri da li ima zahtjeva za pomoc!\n");
+										printf("3.Naplati!\n");
+										printf("4.Odjava!\n");
+										printf("Unesite redni broj opcije: ");
+										scanf("%s", logInput);
+									} while (strcmp(logInput, "4") != 0 && (strcmp(logInput, "1") < 0 || strcmp(logInput, "4") > 0));
+									if (strcmp(logInput, "1") == 0)
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										printD();
+										printf("\n");
+										printRoadStat();
+										system("pause");
+									}
+									else if (strcmp(logInput, "2") == 0)
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										printRoadStat();
+										{
+											printf("Pomoc poslata!\n");
+											SOScount--;
+											system("pause");
+										}
+									}
+									else if (strcmp(logInput, "3") == 0)
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										char sifra[6];
+										BILL ob;
+										do
+										{
+											printf("Unesite sifru vozaceve potvrde:\n");
+											scanf("%s", sifra);//sifra prestavlja sifru potvrde koju dobije korisnik na pocetku
+											ob = findPassword(sifra); //trazi sifru u dadoteci potvrda,i kada nadje samo inicilazuje podatak u mainu
+
+										} while (ob.year == -1);//ovo koristim ako vozac slucajno unese pogresnu sifru,pa da ima mogucnost opet unosa
+										printf("Sifra je pronadjenja:%s\n", ob.password); //pronalaz potvrde!
+										finallprice(&ob, dion); //proracun cjene!
+										speedTicket(&ob, dion);
+										printf("Hvala Vam ste koristili nase usluge!\nSretan put\n");//ispis na displeju sa vanjske strane kucice
+										system("pause");
+									}
+									else if (strcmp(logInput, "4") == 0)
+									{
+										system("pause");
+										break;
+									}
+								}
+							}
 							}
 						}
-					}
+						else
+						{
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							printf("Pokusajte ponovo kasnije!\n");
+							system("pause");
+						}
+						break;
 					}
 				}
-				else
+
+				else if (strcmp(input, "2") == 0)
 				{
 					system("CLS");
 					printf("******************HIGHWAY2016*******************\n\n\n");
-					printf("Pokusajte ponovo kasnije!\n");
+					printf("Sistem generise SOS signal!\n");
+					SOScount++;
 					system("pause");
+					break;
 				}
-				break;
+				else if (strcmp(input, "3") == 0)
+				{
+					system("CLS");
+					printf("******************HIGHWAY2016*******************\n\n\n");
+					printf("Na pocetku birate jednu od ponudjenih opcija u skladu sa Vasim zahtjevima.\nNakon pristupa softveru kao admin ");
+					printf("ili kao operater imate mogucnosti uglavnom \nmanipulisanja podacima i/ili uvid u stanje autoputa. \nZa vise informacija ");
+					printf("otvorite HELP sekciju prirucnika!\n\n\n\n\n");
+					system("pause");
+					break;
+				}
+				else
+					break;
 			}
-		}
-		
-		case(2):
-		{
-			system("CLS");
-			printf("******************HIGHWAY2016*******************\n\n\n");
-			printf("Sistem generise SOS signal!\n");
-			SOScount++;
-			system("pause");
-			break;
-		}
-		case(3):
-		{
-			system("CLS");
-			printf("******************HIGHWAY2016*******************\n\n\n");
-			printf("Na pocetku birate jednu od ponudjenih opcija u skladu sa Vasim zahtjevima.Nakon pristupa softveru kao admin ");
-			printf("ili kao operater imate mogucnosti uglavnom manipulisanja podacima i/ili uvid u stanje autoputa. Za vise informacija ");
-			printf("otvorite HELP sekciju prirucnika!\n");
-			system("pause");
-			break;
-		}
-		case(0):
-			break;
-		}
+		}while (strcmp(input, "1")<0 || strcmp(input, "3")>0);
 	}
 	system("pause");
 
--- Highway2016/Ulaz/Source.c
+++ Highway2016/Ulaz/Source.c
@@ -12,7 +12,8 @@ int main()
 {
 	//ideja je da program funkcionise na racunaru za kojim operise covjek, a nalazi se u kucici, tako da, imacemo kucicu na ulazu i 
 	//izlazu
-	int input = 1, input2, input3 = 1, SOScount = readSOSReport();
+	int SOScount = readSOSReport(), log;
+	char logInput[2], input[2], input2[2];
 	int enteringCount;
 	enteringCount = readReport();
 	char d1, category[5];
@@ -36,73 +37,115 @@ int main()
 			printf("3.Pozovi pomoc na autoput!\n");
 			printf("4.Pomoc pri koristenju programa!\n");
 			printf("\n\nUnesite redni broj opcije:");
-			scanf("%d", &input);
-		} while (input > 4 || input < 1);
-		switch (input)
-		{
-		case(1):
+			scanf("%s", input);
 			{
-				do
-				{
-					system("CLS");
-					printf("******************HIGHWAY2016*******************\n\n\n");
-					printf("1.Admin\n2.Operater\n3.Izlaz\nIzaberite opciju:");
-					scanf("%d", &input2);
-				} while (input2 < 1 || input2>3);
-				int countLogs = 0;
-				if (input2 == 3)break;
-				else
+				if (strcmp(input, "1") == 0)
 				{
 					do
 					{
 						system("CLS");
 						printf("******************HIGHWAY2016*******************\n\n\n");
-						printf("Unesite korisnicko ime:");
-						scanf("%s", name);
-						printf("Unesite lozinku:");
-						scanf("%s", password);
-						countLogs++;
-					} while (LoginCheck(name, password, input2) == 0 && countLogs < 4);
-					if (countLogs < 4)
+						printf("1.Admin\n2.Operater\n3.Izlaz\nIzaberite opciju:");
+						scanf("%s", input2);
+					} while (strcmp(input2, "1") < 0 || strcmp(input2, "3") > 0);
+					int countLogs = 0;
+					if (strcmp(input2, "3") == 0)break;
+					else
 					{
-						system("CLS");
-						printf("******************HIGHWAY2016*******************\n\n\n");
-						switch (input2)
+						do
 						{
-						case(1):
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							if (strcmp(input2, "1") == 0)
+								log = 1;
+							else log = 2;
+							printf("Unesite korisnicko ime:");
+							scanf("%s", name);
+							printf("Unesite lozinku:");
+							scanf("%s", password);
+							countLogs++;
+						} while (LoginCheck(name, password, log) == 0 && countLogs < 4);
+						if (countLogs < 4)
 						{
-							do
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							switch (log)
 							{
-								while (1)
+							case(1):
+							{
+								do
 								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									printf("MENI:\n\n");
-									printf("2.Promijeni stanje na putevima!\n");
-									printf("3.Izlistaj trenutna stanja na dionicama!\n");
-									printf("4.Dodaj novog zaposlenog!\n");
-									printf("5.Izlaz iz programa!\n");
-									printf("6.Odjava!\n");
-									printf("Unesite redni broj opcije:");
-									scanf("%d", &input3);
-									 if (input3 == 2)
+									while (1)
 									{
 										system("CLS");
 										printf("******************HIGHWAY2016*******************\n\n\n");
-										char workBool[3];
-										char traficBool[3];
-										printf("Na dionici %s se odvijaju radovi?(da/ne)", dion);
-										scanf("%s", workBool);
-										if (!strcmp(workBool, "da"))
+										printf("MENI:\n\n");
+										printf("1.Promijeni stanje na putevima!\n");
+										printf("2.Izlistaj trenutna stanja na dionicama!\n");
+										printf("3.Dodaj novog zaposlenog!\n");
+										printf("4.Izlaz iz programa!\n");
+										printf("5.Odjava!\n");
+										printf("Unesite redni broj opcije:");
+										scanf("%s", logInput);
+										if (strcmp(logInput, "1") == 0)
 										{
-											add_maintanceWorker(dion[0]);
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											printf("Na kojoj putanji se vrsi izmjena(putanja oznacava dvije dionice):\n");
+											char dio1[2], dio2[2];
+											scanf("%s %s", dio1, dio2);//trazi da uneses dvije dionice da pormenis njhova stanja!!
+											changeStat(dio1, dio2);
 										}
-										printf("Na kojoj putanji se vrsi izmjena(putanja oznacava dvije dionice):\n");
-										char dio1[2], dio2[2];
-										scanf("%s %s", dio1, dio2);//trazi da uneses dvije dionice da pormenis njhova stanja!!
-										changeStat(dio1, dio2);
+										else if (strcmp(logInput, "2") == 0)
+										{
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											printD();
+											printf("\n");
+											printRoadStat();
+											system("pause");
+										}
+										else if (strcmp(logInput, "3") == 0)
+										{
+											system("CLS");
+											printf("******************HIGHWAY2016*******************\n\n\n");
+											addWorker();
+										}
+										else if (strcmp(logInput, "4") == 0)
+										{
+											fixReport(enteringCount);
+											fixSOSReport(SOScount);
+											system("pause");
+											return 0;
+										}
+										else if (strcmp(logInput, "5") == 0)
+										{
+											system("pause");
+											break;
+										}
 									}
-									else if (input3 == 3)
+								} while (strcmp(logInput, "5") != 0 && (strcmp(logInput, "1") < 0 || strcmp(logInput, "1") > 0));
+								break;
+
+
+
+							}
+							case(2):											//opcije operatera,dovrsiti
+							{
+								while (1)
+								{
+									do
+									{
+										system("CLS");
+										printf("******************HIGHWAY2016*******************\n\n\n");
+										printf("MENI:\n\n");
+										printf("1.Izlistaj stanja na dionicama!\n");
+										printf("2.Provjeri da li ima zahtjeva za pomoc!\n");
+										printf("3.Odjava!\n");
+										printf("Unesite redni broj opcije: ");
+										scanf("%s", logInput);
+									} while (strcmp(logInput, "3") != 0 && (strcmp(logInput, "1") < 0 || strcmp(logInput, "3") > 0));
+									if (strcmp(logInput, "1") == 0)
 									{
 										system("CLS");
 										printf("******************HIGHWAY2016*******************\n\n\n");
@@ -111,129 +154,81 @@ int main()
 										printRoadStat();
 										system("pause");
 									}
-									else if (input3 == 4)
+									else if (strcmp(logInput, "2") == 0)
 									{
 										system("CLS");
 										printf("******************HIGHWAY2016*******************\n\n\n");
-										addWorker();
+										if (SOScount == 0)
+										{
+											printf("Nema zahtjeva!\n");
+											system("pause");
+										}
+										else
+										{
+											printf("Pomoc poslata!\n");
+											SOScount--;
+											system("pause");
+										}
 									}
-									else if (input3 == 5)
-									{
-										fixReport(enteringCount);
-										fixSOSReport(SOScount);
-										system("pause");
-										return 0;
-									}
-									else if (input3 == 6)
+									else if (strcmp(logInput, "3") == 0)
 									{
 										system("pause");
 										break;
 									}
 								}
-							} while (input3 < 1 || input3 >6);
-							break;
-
-
-
-						}
-						case(2):											//opcije operatera,dovrsiti
-						{
-							while (1)
-							{
-								do
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									printf("MENI:\n\n");
-									printf("1.Izlistaj stanja na dionicama!\n");
-									printf("2.Provjeri da li ima zahtjeva za pomoc!\n");
-									printf("3.Odjava!\n");
-									printf("Unesite redni broj opcije: ");
-									scanf("%d", &input3);
-								} while (input3 < 1 || input3 >3);
-								if (input3 == 1)
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									printD();
-									printf("\n");
-									printRoadStat();
-									system("pause");
-								}
-								else if (input3 == 2)
-								{
-									system("CLS");
-									printf("******************HIGHWAY2016*******************\n\n\n");
-									if (SOScount == 0)
-									{
-										printf("Nema zahtjeva!\n");
-										system("pause");
-									}
-									else
-									{
-										printf("Pomoc poslata!\n");
-										SOScount--;
-										system("pause");
-									}
-								}
-								else if (input3 == 3)
-								{
-									system("pause");
-									break;
-								}
+							}
 							}
 						}
+						else
+						{
+							system("CLS");
+							printf("******************HIGHWAY2016*******************\n\n\n");
+							printf("Pokusajte ponovo kasnije!\n");
+							system("pause");
 						}
+						break;
 					}
-					else
-					{
-						system("CLS");
-						printf("******************HIGHWAY2016*******************\n\n\n");
-						printf("Pokusajte ponovo kasnije!\n");
-						system("pause");
-					}
+				}
+				else if (strcmp(input, "2") == 0)
+				{
+					system("CLS");
+					printf("******************HIGHWAY2016*******************\n\n\n");
+					printf("Unesite kategoriju vozila:\n");
+					printf("Automobil - A\n");
+					printf("Autobus   - B\n");
+					printf("Kamion    - K\n");
+					printf("Motocikl  - M\n");
+					scanf("%s", category);
+					printf("Potvrda se stampa!\n");
+					enteringCount++;
+					printing(dion[0], enteringCount, category[0]);
+					printf("Preuzmite potvrdu!\nMolimo vas zapamtite sifru: %05d\n", enteringCount);
+					system("pause");
 					break;
 				}
-}
-		case(2):
-			{
-				system("CLS");
-				printf("******************HIGHWAY2016*******************\n\n\n");
-				printf("Unesite kategoriju vozila:\n");
-				printf("Automobil - A\n");
-				printf("Autobus   - B\n");
-				printf("Kamion    - K\n");
-				printf("Motocikl  - M\n");
-				scanf("%s", category);
-				printf("Potvrda se stampa!\n");
-				enteringCount++;
-				printing(dion[0], enteringCount, category[0]);
-				printf("Preuzmite potvrdu!\nMolimo vas zapamtite sifru: %05d\n",enteringCount);
-				system("pause");
-				break;
+				else if (strcmp(input, "3") == 0)
+				{
+					system("CLS");
+					printf("******************HIGHWAY2016*******************\n\n\n");
+					printf("Softver generise SOS signal!\n\nSOS signal uspjesno poslat.\nUkoliko pomoc blagovremeno ne stigne, potrazite najblizi SOS telefon na autoputu.");
+					SOScount++;
+					system("pause");
+					break;
+				}
+				else if (strcmp(input, "4") == 0)
+				{
+					system("CLS");
+					printf("******************HIGHWAY2016*******************\n\n\n");
+					printf("Na pocetku birate jednu od ponudjenih opcija u skladu sa Vasim zahtjevima.\nNakon pristupa softveru kao admin ");
+					printf("ili kao operater imate mogucnosti uglavnom \nmanipulisanja podacima i/ili uvid u stanje autoputa. \nZa vise informacija ");
+					printf("otvorite HELP sekciju prirucnika!\n\n\n\n\n\n\n");
+					system("pause");
+					break;
+				}
+				else
+					break;
 			}
-		case(3):
-			{
-				system("CLS");
-				printf("******************HIGHWAY2016*******************\n\n\n");
-				printf("Softver generise SOS signal!\n\nSOS signal uspjesno poslat.\nUkoliko pomoc blagovremeno ne stigne, potrazite najblizi SOS telefon na autoputu.");
-				SOScount++;
-				system("pause");
-				break;
-			}
-		case(4):
-			{
-				system("CLS");
-				printf("******************HIGHWAY2016*******************\n\n\n");
-				printf("Na pocetku birate jednu od ponudjenih opcija u skladu sa Vasim zahtjevima.Nakon pristupa softveru kao admin ");
-				printf("ili kao operater imate mogucnosti uglavnom manipulisanja podacima i/ili uvid u stanje autoputa. Za vise informacija ");
-				printf("otvorite HELP sekciju prirucnika!\n");
-				system("pause");
-				break;
-			}
-		case(0):
-			break;
-		}
+		} while (strcmp(input, "1") < 0 || strcmp(input, "4") > 0);
 	}
 	system("pause");
 
